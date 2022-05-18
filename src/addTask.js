2e1d3a8fb58cdb4fc6ce7deb5a06dba7509a2478
Luis Guisado <lguisadom@gmail.com>
2022-05-18 01:53:25 -0500

    Se agrega middleware: modulo middy

--- src/addTask.js
+++ src/addTask.js
@@ -1,10 +1,12 @@
 const { v4 } = require('uuid');
 const AWS = require('aws-sdk');
-const addTask = async (event) => {
+const middy = require('@middy/core');
+const jsonBodyParser = require('@middy/http-json-body-parser');
 
+const addTask = async (event) => {
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-    const { title, description } = JSON.parse(event.body);
+    const { title, description } = event.body;
     const createdAt = new Date();
     const id = v4();
 
@@ -28,5 +30,5 @@ const addTask = async (event) => {
 }
 
 module.exports = {
-    addTask
+    addTask: middy(addTask).use(jsonBodyParser()),
 }
